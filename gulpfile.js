9b5713b4379f8402348cd1354f429980de953bb5
Edward Khor <edward@enfonius.com>
2017-10-16 14:36:57 +0800

    added templates gulp task to copy the folder over the dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const copy = require('gulp-copy');
 const notify = require('gulp-notify');
 
 
@@ -13,9 +14,13 @@ gulp.task('foundation', () => gulp.src('foundation/**/*.{js,jsx}')
   .pipe(gulp.dest('dist/foundation'))
   .pipe(notify({ message: 'foundation/**/* compiled successfully', onLast: true })));
 
+gulp.task('templates', () => gulp.src('templates/**/*')
+  .pipe(copy('./dist/')));
+
 gulp.task('watch-binary', () => gulp.watch('bin/**/*', gulp.parallel('binary')));
 gulp.task('watch-foundation', () => gulp.watch('foundation/**/*', gulp.parallel('foundation')));
+gulp.task('watch-templates', () => gulp.watch('templates/**/*', gulp.parallel('templates')));
 
-gulp.task('default', gulp.parallel('binary', 'foundation'));
-gulp.task('watch', gulp.parallel('default', 'watch-binary', 'watch-foundation'));
+gulp.task('default', gulp.parallel('binary', 'foundation', 'templates'));
+gulp.task('watch', gulp.parallel('default', 'watch-binary', 'watch-foundation', 'watch-templates'));
 
