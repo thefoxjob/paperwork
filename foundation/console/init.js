d61509668cb9a4d4cee40c407141a28b01591a1c
Edward Khor <edward@enfonius.com>
2017-10-20 08:59:36 +0700

    fixed console hmr error and copy path command bugs

--- foundation/console/init.js
+++ foundation/console/init.js
@@ -1,9 +1,13 @@
-import copyfiles from 'copyfiles';
+import fs from 'fs-extra';
 import path from 'path';
 
 
 const execute = () => {
-  copyfiles([path.resolve(__dirname, './templates'), path.resolve(process.cwd(), './templates')], () => {
+  fs.copy(path.resolve(__dirname, '../../templates'), path.resolve(process.cwd(), './templates'), (error) => {
+    if (error) {
+      throw error;
+    }
+
     // eslint-disable-next-line no-console
     console.info(`${ path.resolve(__dirname, './templates') } > ${ path.resolve(process.cwd(), './templates') }`);
   });
