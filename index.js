3f6ea8108e36de894aa52ddcad19d8d6fbf1abd6
Edward Khor <edward@enfonius.com>
2017-10-29 03:52:49 +0800

    remove currency from package

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import auth from './foundation/auth';
-import currency from './foundation/currency';
 import log from './foundation/log';
 import service from './foundation/service';
 
-export { auth, currency, log, service };
+export { auth,  log, service };
