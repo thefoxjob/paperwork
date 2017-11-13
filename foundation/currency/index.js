e41716c1371cd5a33b4c8cdfdaf1639d1b6e5c50
Edward Khor <edward@enfonius.com>
2017-11-13 22:36:18 +0800

    expose CurrencyAdapter

---
+++ foundation/currency/index.js
@@ -0,0 +1,6 @@
+import CurrencyAdapter from './CurrencyAdapter';
+import CurrencyServiceProvider from './CurrencyServiceProvider';
+import Currency from './Currency';
+
+
+export default { CurrencyAdapter, CurrencyServiceProvider, Currency };
--- foundation/index.js
+++ foundation/index.js
@@ -2,5 +2,6 @@ import config from './config';
 import log from './log';
 import relay from './graphql/relay';
 import { AuthAdapter } from './auth';
+import { CurrencyAdapter } from './currency';
 
-export { AuthAdapter, config, log, relay };
+export { AuthAdapter, CurrencyAdapter, config, log, relay };
